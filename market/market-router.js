a8dd23282a81aa196a178ddc45ca74c3e2125312
William <billy.ryan.rom@gmail.com>
2021-04-15 10:03:40 -0400

    Add api routing to market-router.js for market table

--- market/market-router.js
+++ market/market-router.js
@@ -1,42 +1,92 @@
-const db = require('../data/dbConfig.js')
+const express = require('express');
+const Market = require('./market-model')
 
-module.exports = {
-    findAll,
-    findById,
-    add,
-    update,
-    removeAll,
-    removeById
-}
+const router = express.Router()
 
-function findAll(){
-    return db('market')
-}
+router.get('/market', (req, res) => {
+    
+    Market.findAll()
+    .then(market => {
+        res.status(200).json(market)
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving market columns' })
+    })
+})
 
-function findById(id) {
-    return db('market')
-        .where({ id })
-        .first()
-}
+router.get('/market/:id', (req, res) => {
+    const id  = req.params.id;
 
-function add(data){
-    return db('market')
-        .insert(data)
-}
+    Market.findById(id)
+    .then(market => {
+        if(market.id >= 0){
+            return res.status(200).json(market)
+        } else {
+            return res.status(404).json({ message: 'Error id invalid' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving market column' })
+    })
+})
 
-function update(changes, id){
-    return db('market')
-        .update(changes)
-        .where({ id })
-}
+router.post('/market', (req, res) => {
+    const data = req.body;
 
-function removeAll(){
-    return db('market')
-        .del()
-}
+    Market.add(data)
+    .then(market => {
+        res.status(201).json(market)
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Failed to Create market column'})
+    })
+})
 
-function removeById(id){
-    return db('market')
-        .where({ id })
-        .del()
-}
+router.put('/market/:id', (req, res) => {
+    const id  = req.params.id;
+    const changes = req.body;
+
+    Market.findById(id)
+    .then(market => {
+        if (market){
+            Market.update(changes, id)
+            .then(updatedMarket => {
+                res.status(200).json(updatedMarket)
+            });
+        } else {
+            res.status(404).json({ message: 'Could not find specified market column'})
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error updating market column' })
+    })
+})
+
+router.delete('/market', (req, res) => {
+
+    Market.removeAll()
+    .then(() => {
+        res.status(200).json({ message: 'All market columns successfully deleted' })
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Unable to delete market columns' })
+    })
+})
+
+router.delete('/market/:id', (req, res) => {
+    const id  = req.params.id;
+
+    Market.removeById(id)
+    .then(deleted => {
+        if(deleted) {
+            res.status(200).json({ message: 'market column removed' })
+        } else {
+            res.status(404).json({ message: 'Could not find specified market column' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error deleting market column' })
+    })
+})
+
+module.exports = router
